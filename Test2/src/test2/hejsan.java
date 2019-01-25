ef8bf543d5ae9a54ab03e0a4e74318e9f6c2b997
aBeskow <47024539+aBeskow@users.noreply.github.com>
2019-01-25 14:07:13 +0100

    Klar

--- Test2/src/test2/hejsan.java
+++ Test2/src/test2/hejsan.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hejsan;
+package test2;
 
 /**
  *
- * @author User
+ * @author Adam
  */
-public class Hejhej {
+public class hejsan {
     
 }
