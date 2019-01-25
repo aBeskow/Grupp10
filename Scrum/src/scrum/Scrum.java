79a4278a3ce33978dc86a2dd0b8c08749b1ebf98
Bergssen <Nikola.Bergsen@outlook.com>
2019-01-25 14:06:13 +0100

    lol

---
+++ Scrum/src/scrum/Scrum.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scrum;
+
+/**
+ *
+ * @author Adam
+ */
+public class Scrum {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+<<<<<<< HEAD:Scrum/src/scrum/Scrum.java
+        // TODO code application logic here
+=======
+   
+        System.out.println("HEJHEJ!!");
+        System.out.println("HEEEEJ");
+>>>>>>> e3dc88786667a3bbb31840d72fe4ddb1db9bfed9:Hejsan/src/hejsan/Hejsan.java
+    }
+    
+}
